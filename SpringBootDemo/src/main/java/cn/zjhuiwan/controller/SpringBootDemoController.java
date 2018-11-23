ef4bbfd4e3fd5faee61b7041b64e3a4125dc8d06
sunonzj <sunonzj128@gmail.com>
2018-11-23 10:17:50 +0800

    修改了输出

--- SpringBootDemo/src/main/java/cn/zjhuiwan/controller/SpringBootDemoController.java
+++ SpringBootDemo/src/main/java/cn/zjhuiwan/controller/SpringBootDemoController.java
@@ -6,8 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController // 是spring4里的新注解，是@ResponseBody和@Controller的缩写
 public class SpringBootDemoController {
     @RequestMapping("/hello")
-    public String hello() {
-        return "HELLO SpringBoot! 这是一个springboot的demo<p>这是一个测试赛</p>欢迎大家访问我的个人博客：http://www.zjhuiwan.cn<a href='http://www.zjhuiwan.cn'>ZJBLOG</a>";
+    public String hello(String name) {
+        return name
+            + "HELLO SpringBoot! 这是一个springboot的demo<p>这是一个测试赛</p>欢迎大家访问我的个人博客：http://www.zjhuiwan.cn<a href='http://www.zjhuiwan.cn'>ZJBLOG</a>";
     }
 
 }
